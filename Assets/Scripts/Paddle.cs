298234c0418f056ff69d5bba6469b45d588e0567
ricardoham <rdomanoel@gmail.com>
2019-12-22 05:09:15 -0300

    add new script

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -7,11 +7,8 @@ public class Paddle : MonoBehaviour
     [SerializeField]  float screenWidthInUnits = 16f;
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Start is called before the first frame update      
+    
 
     // Update is called once per frame
     void Update()
@@ -20,5 +17,8 @@ public class Paddle : MonoBehaviour
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
         paddlePos.x = Mathf.Clamp(mousePosition, minX, maxX);
         transform.position = paddlePos;
-    }
+
+        }
 }
+// Loop funcao sem fim
+// termina apenas no final de uma condiçao

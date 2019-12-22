3ad147c005097db0da21eadb351deb8257d89003
ricardoham <rdomanoel@gmail.com>
2019-12-22 05:37:24 -0300

    ball fallow paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] Paddle paddle1;
+
+// Vector 2 represents the 2D vectors and points
+    Vector2 paddleToBallVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
     }
 }
